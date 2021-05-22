4ddeb73e7610a3a39529738c08484baa6a728781
Koch Christopher <koch.christopher@hotmail.fr>
2021-05-22 17:04:34 +0200

    factorisation code js

--- js/photograph.js
+++ js/photograph.js
@@ -1,3 +1,37 @@
+ //cible les éléments pour y mettre les données   
+ const photoName = document.getElementById("photographer_name");
+ const photoLocation = document.getElementById("photographer_location");
+ const photoDescription = document.getElementById("photographer_description");
+ const tagContainer= document.getElementsByClassName("main__presentation__photograph__tagsContainer");
+ const photoPicture = document.getElementById("photographer_picture");
+ const nameForm = document.getElementById("name_Modale")
+ var pictureDiv = document.getElementsByClassName("main__photos__article");
+ let heartFull = document.getElementsByClassName("heart_full");
+ let photoContainerImage= document.getElementsByClassName("main__photos__article__container__img")
+  let bigPicture= document.getElementsByClassName("big-picture")
+  let bigPictureClose= document.getElementsByClassName("big-picture__container__cross")
+  let bigPictureImg= document.getElementsByClassName("big-picture__container__img")
+  let header = document.getElementsByTagName("header");
+  let main = document.getElementsByTagName("main")
+  let bigPictureTitle= document.getElementsByClassName("big-picture__container__title")
+  let photoContainer= document.getElementsByClassName("main__photos__article__container")
+  let priceTag= document.getElementsByClassName("main__like-price-button__price");
+  let leftArrow= document.getElementsByClassName("big-picture__container__left-arrow");
+  let rightArrow= document.getElementsByClassName("big-picture__container__right-arrow");
+  let totalLike= document.getElementsByClassName("main__like-price-button__number");
+  const likeButton = document.getElementsByClassName("main__photos__article__container__description__heart");
+  const likeCount = document.getElementsByClassName("main__photos__article__container__description__like");
+  let dateButton= document.getElementsByClassName("main__photos__allbutton__button__date");
+  let titleButton= document.getElementsByClassName("main__photos__allbutton__button__title");
+  let popButton= document.getElementsByClassName("main__photos__allbutton__button__popular")
+  
+ // récupère l'id dans l'url
+ let urlId = window.location.search.slice(1);
+
+
+
+
+
 
 fetch('js/FishEyeData.json')
   .then(function(res) {
@@ -5,81 +39,40 @@ fetch('js/FishEyeData.json')
       return res.json(); 
     } else {alert ("test")}
   })
-  .then(function(value) {
 
-  // récupère l'id dans l'url
-    let urlId = window.location.search.slice(1);
+.then(function(value) {
 
   //cible les média qqui ont l'id correspondant
-  let test3= value.photographers.find(x => x.id == urlId);
-   //cible les éléments pour y mettre les données   
-   const photoName = document.getElementById("photographer_name");
-   const photoLocation = document.getElementById("photographer_location");
-   const photoDescription = document.getElementById("photographer_description");
-   const tagContainer= document.getElementsByClassName("main__presentation__photograph__tagsContainer");
-   const photoPicture = document.getElementById("photographer_picture");
-   const nameForm = document.getElementById("name_Modale")
-   var pictureDiv = document.getElementsByClassName("main__photos__article");
-   let heartFull = document.getElementsByClassName("heart_full");
-   let photoContainerImage= document.getElementsByClassName("main__photos__article__container__img")
-    let bigPicture= document.getElementsByClassName("big-picture")
-    let bigPictureClose= document.getElementsByClassName("big-picture__container__cross")
-    let bigPictureImg= document.getElementsByClassName("big-picture__container__img")
-    let header = document.getElementsByTagName("header");
-    let main = document.getElementsByTagName("main")
-    let bigPictureTitle= document.getElementsByClassName("big-picture__container__title")
-    let photoContainer= document.getElementsByClassName("main__photos__article__container")
-    let priceTag= document.getElementsByClassName("main__like-price-button__price");
-    let leftArrow= document.getElementsByClassName("big-picture__container__left-arrow");
-    let rightArrow= document.getElementsByClassName("big-picture__container__right-arrow");
-
-
+    let test3= value.photographers.find(x => x.id == urlId);
+  
   //cible les média par rapport au id du photographe
-  let numberOfPicture= value.media.filter(x => x.photographerId == urlId);
-  let totalLike= document.getElementsByClassName("main__like-price-button__number");
-  const likeButton = document.getElementsByClassName("main__photos__article__container__description__heart");
-  const likeCount = document.getElementsByClassName("main__photos__article__container__description__like");
-
-   //insérer les données du json pour remplir la page
-   photoName.innerHTML = test3.name;
-   photoLocation.innerHTML= test3.city+","+test3.country;
-   photoDescription.innerHTML= test3.tagline;
-   photoPicture.setAttribute("src", "/images/sample photos/Photographers ID Photos/"+test3.portrait)
-   var numberOfPhotographersTags = test3.tags.length;
-   nameForm.innerHTML= "Contactez-moi" + "<br />" +test3.name
-   
-   //créer les tags selon leur nombre
-   for (var x=0; x < numberOfPhotographersTags; x++) {
-         
-    var tags2 = document.createElement("div");
-    tagContainer[0].appendChild(tags2);
-    tags2.classList.add("main__presentation__photograph__tags");
-    tags2.innerHTML= "#"+test3.tags[x];
+    let numberOfPicture= value.media.filter(x => x.photographerId == urlId);
+  
+  //insérer les données du json pour remplir la page
+    photoName.innerHTML = test3.name;
+    photoLocation.innerHTML= test3.city+","+test3.country;
+    photoDescription.innerHTML= test3.tagline;
+    photoPicture.setAttribute("src", "/images/sample photos/Photographers ID Photos/"+test3.portrait)
+    var numberOfPhotographersTags = test3.tags.length;
+    nameForm.innerHTML= "Contactez-moi" + "<br />" +test3.name
     
+  //créer les tags selon leur nombre
+   for (var x=0; x < numberOfPhotographersTags; x++) {   
+      var tags2 = document.createElement("div");
+      tagContainer[0].appendChild(tags2);
+      tags2.classList.add("main__presentation__photograph__tags");
+      tags2.innerHTML= "#"+test3.tags[x];
    }
   // remplir le bouton rose prix
-      
       priceTag[0].innerHTML= test3.price+ "€/jour";
 
   //garde le prénom du photographe pour créer le lien vers leur dossier image
     var spaceLastname = test3.name.indexOf(" ");
     var lastname = test3.name.substr(0,spaceLastname);
 
-
    //tableau 
-   var resultLike = 0
-  //// Trier selon le nombre de likes////
-   function compareLikes(a,b){
-     if (a.likes < b.likes)
-     return 0
-     if (a.likes >b.likes)
-     return -1;
-   }
-   numberOfPicture.sort(compareLikes);
-   //
+   var resultLike =0;
   
-
-
 // structure des images 
    function mediaMaker(p){
     
@@ -119,10 +112,7 @@ fetch('js/FishEyeData.json')
       newI2.classList.add("far");
       newI2.classList.add("fa-heart")
       newI2.classList.add("heart_empty")
-  
   }
-
-
 
    /////boucle création de média +likes///////////////////////////////////////////////////////////////////////////////////
 
@@ -189,25 +179,7 @@ function imgListener(i){
     })
 }
 
-pageMedia();
-for (var i=0; i < numberOfPicture.length; i++){
-  (function (i){
-    likeClick(i)
-     
-  })
-  (i)
-};
-for (var i=0; i<numberOfPicture.length;i++) {
-  
-  (function (i){
-    
-   imgListener(i);
-    
-    
-  })(i)
-  }
-  
-  //FIN BOUCLE
+
 ////function qui détruit les médias créés
 
 
@@ -215,79 +187,78 @@ function deleteMedia(){
   
   for (d=0; d< numberOfPicture.length; d++){
    photoContainer[0].remove()
-
   }
+}
+
+//////////////////////////////////////////////////////////////////////
+///// Bouton filtre par titre!////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
 
-}
-  ///// Bouton filtre par titre!////////
-let dateButton= document.getElementsByClassName("main__photos__allbutton__button__date");
-let titleButton= document.getElementsByClassName("main__photos__allbutton__button__title");
-let popButton= document.getElementsByClassName("main__photos__allbutton__button__popular")
-//function filtre les objet selon leur titre
-function compareTitle(a,b){
-  if (a.title < b.title)
-  return -1;
-  if (a.title >b.title)
-  return 0;
-}
-///EventListener du bouton titre qui trie les objets et les recrééer
-titleButton[0].addEventListener("click",function(){
+function filterBy(e) {
   resultLike=0;
-        numberOfPicture.sort(compareTitle)
-        deleteMedia()
-        pageMedia();
-
-        for (var i=0; i<numberOfPicture.length;i++) {
-          (function (i){
-          imgListener(i);
-          likeClick(i)
-          })(i)
-          }
-})
-//
-
-
-
-
-//function filtre les objets selon leur DATE
-
-function compareDate(a,b){
-  return new Date(b.date) - new Date(a.date)
+  numberOfPicture.sort(e)
+  deleteMedia()
+  pageMedia();
+  for (var i=0; i<numberOfPicture.length;i++) {
+    (function (i){
+    imgListener(i);
+    likeClick(i)
+    })(i)
+    }
 }
-///EventListener du bouton date qui trie les objets et les recrééer
-dateButton[0].addEventListener("click",function(){
-  resultLike=0;
-      numberOfPicture.sort(compareDate)
-      deleteMedia()
-      pageMedia();
-      for (var i=0; i<numberOfPicture.length;i++) {
-        (function (i){
-        imgListener(i);
-        likeClick(i)
-        })(i)
-        }
+      //function filtre les objet selon leur titre
+      function compareTitle(a,b){
+        if (a.title < b.title)
+        return -1;
+        if (a.title >b.title)
+        return 0;
+      }
+      ///EventListener du bouton titre qui trie les objets et les recrééer
+      titleButton[0].addEventListener("click",function(){
+          filterBy(compareTitle)
+      })
+    
+
+      //function filtre les objets selon leur DATE
+      function compareDate(a,b){
+        return new Date(b.date) - new Date(a.date)
+      }
+      ///EventListener du bouton date qui trie les objets et les recrééer
+      dateButton[0].addEventListener("click",function(){
+        filterBy(compareDate)
+        })
+
+
+      //// Trier selon le nombre de likes////
+      function compareLikes(a,b){
+        if (a.likes < b.likes)
+        return 0
+        if (a.likes >b.likes)
+        return -1;
+      }
+      
+      ///EventListener du bouton populaire qui trie les objets et les recrééer
+      popButton[0].addEventListener("click",function(){
+        filterBy(compareLikes);
+        })
+
+
+//Function qui se lance au lancement de la page
+
+numberOfPicture.sort(compareLikes);
+pageMedia();
+for (var i=0; i < numberOfPicture.length; i++){
+  (function (i){
+    likeClick(i);
+    imgListener(i);
   })
-  //
+  (i)
+};
 
-
-///EventListener du bouton populaire qui trie les objets et les recrééer
-popButton[0].addEventListener("click",function(){
-  resultLike=0;
-      numberOfPicture.sort(compareLikes)
-      deleteMedia()
-      pageMedia();
-      for (var i=0; i<numberOfPicture.length;i++) {
-        (function (i){
-        imgListener(i);
-        likeClick(i)
-        })(i)
-        }
-  })
-
-
-
-
+//////////////////////////////////////////////////////////////////////
+//////ZONE IMAGE AGRANDIE/////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 //ferme l'image
 bigPictureClose[0].addEventListener("click",function(){
 
@@ -295,8 +266,10 @@ bigPictureClose[0].addEventListener("click",function(){
   header[0].style.opacity="1";
     main[0].style.opacity="1";
 })
-//ouvre l'image
-
+function srcTitle(){
+  bigPictureImg[0].src= photoContainerImage[imageResult].src;
+  bigPictureTitle[0].innerHTML= numberOfPicture[imageResult].title ;
+}
 
 
 function videoInMedia () {
@@ -319,15 +292,11 @@ leftArrow[0].addEventListener("click",function(){
 
         if(imageResult<=0){
           imageResult += numberOfPicture.length -1;
-          bigPictureImg[0].src= photoContainerImage[imageResult].src;
-        bigPictureTitle[0].innerHTML= numberOfPicture[imageResult].title ;
         }else {
         imageResult --
-        bigPictureImg[0].src= photoContainerImage[imageResult].src;
-        bigPictureTitle[0].innerHTML= numberOfPicture[imageResult].title ;
-        
         }
         videoInMedia()
+        srcTitle()
   
 })
 
@@ -335,14 +304,10 @@ rightArrow[0].addEventListener("click",function(){
   
       if (imageResult==numberOfPicture.length -1){
         imageResult =0;
-        console.log(imageResult)
-        bigPictureImg[0].src= photoContainerImage[0].src;
-        bigPictureTitle[0].innerHTML= numberOfPicture[0].title ;
         }else {
       imageResult ++
-      bigPictureImg[0].src= photoContainerImage[imageResult].src
-      bigPictureTitle[0].innerHTML= numberOfPicture[imageResult].title
       }
+      srcTitle()
       videoInMedia()
 })
 
